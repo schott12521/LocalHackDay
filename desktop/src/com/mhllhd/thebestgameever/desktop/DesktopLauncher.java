4665b0e619b84c4f604cfcae79cf6a1ee931acb6
schott12521 <schott12521@gmail.com>
2015-10-10 10:52:12 -0400

    Initial Commit

---
+++ desktop/src/com/mhllhd/thebestgameever/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mhllhd.thebestgameever.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mhllhd.thebestgameever.MainGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGameClass(), config);
+	}
+}
---
+++ html/src/com/mhllhd/thebestgameever/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mhllhd.thebestgameever.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mhllhd.thebestgameever.MainGameClass;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MainGameClass();
+        }
+}
