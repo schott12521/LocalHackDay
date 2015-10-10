8ce25f617638a04d11eade086f5414445d333d8c
schott12521 <schott12521@gmail.com>
2015-10-10 16:52:58 -0400

    Background

--- core/src/com/mhllhd/thebestgameever/MainGameClass.java
+++ core/src/com/mhllhd/thebestgameever/MainGameClass.java
@@ -12,6 +12,8 @@ import entities.Player;
 import screen.Screen;
 import screen.ScreenManager;
 
+import javax.xml.soap.Text;
+
 /**
  * This should be the root game class. Are we commited? Yes!
  */
@@ -43,15 +45,57 @@ public class MainGameClass extends ApplicationAdapter {
         mainCharacter.move();
 
         batch.begin();
+        drawBackground(batch);
         batch.draw(mainCharacter.getSprite(), mainCharacter.getxPos(), mainCharacter.getyPos(), 100, 100);
         for (int i = 0; i < mainCharacter.bulletArrayList.size(); i++) {
             Bullet temp = ((Bullet) mainCharacter.bulletArrayList.get(i));
             temp.update();
-            batch.draw(temp.getSprite(), temp.getxPos(), temp.getyPos(), 50, 50);
+            batch.draw(temp.getSprite(), temp.getxPos() + 25, temp.getyPos() + 25, 50, 50);
         }
+
         batch.end();
 
 	}
+
+    public void drawBackground(SpriteBatch batch) {
+        Texture frontWall = new Texture(Gdx.files.internal("frontWall.png"));
+        Texture backWall = new Texture(Gdx.files.internal("backWall.png"));
+        Texture floor = new Texture(Gdx.files.internal("floor.png"));
+        Texture leftWall = new Texture(Gdx.files.internal("leftWall.png"));
+        Texture rightWall = new Texture(Gdx.files.internal("rightwall.png"));
+        Texture corner = new Texture(Gdx.files.internal("corner.png"));
+
+        Sprite frontWallSprite = new Sprite(frontWall);
+        Sprite backWallSprite = new Sprite(backWall);
+        Sprite floorSprite = new Sprite(floor);
+        Sprite leftWallSprite = new Sprite(leftWall);
+        Sprite rightWallSprite = new Sprite(rightWall);
+        Sprite cornerSprite = new Sprite(corner);
+
+
+        for (int i = 0; i < WIDTH; i += 32) {
+            for (int j = 0; j < HEIGHT; j += 32)
+                batch.draw(floorSprite, i, j);
+        }
+        for (int i = 0; i < WIDTH; i += 32) {
+            batch.draw(backWallSprite, i, HEIGHT - 64);
+            batch.draw(frontWallSprite, i, 0);
+        }
+        for (int i = 0; i < HEIGHT; i += 32) {
+            batch.draw(leftWallSprite, 0, i);
+            batch.draw(rightWallSprite, WIDTH - 64, i);
+        }
+        batch.draw(cornerSprite, 0, HEIGHT - 64);
+        cornerSprite.setRotation(270f);
+        cornerSprite.setPosition(WIDTH - 64, HEIGHT - 64);
+        cornerSprite.draw(batch);
+        cornerSprite.setRotation(180f);
+        cornerSprite.setPosition(WIDTH - 64, -32);
+        cornerSprite.draw(batch);
+        cornerSprite.setRotation(90f);
+        cornerSprite.setPosition(0, -32);
+        cornerSprite.draw(batch);
+    }
 
     @Override
     public void dispose() {
--- core/src/entities/Player.java
+++ core/src/entities/Player.java
@@ -18,6 +18,7 @@ public class Player {
     private int xPos = MainGameClass.WIDTH / 2 - 50, yPos = MainGameClass.HEIGHT / 2 - 50;
 
     public ArrayList bulletArrayList = new ArrayList();
+    private int direction = 4; // 1 = RIGHT, 2 = LEFT, 3 UP, 4 = DOWN
 
     public Player(SpriteBatch batchIn) {
         texture = new Texture(Gdx.files.internal("littleGuy.png"));
@@ -35,41 +36,65 @@ public class Player {
 
     // TODO handle diagonal moving...
     public void move() {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            Bullet bullet = new Bullet(this.xPos, this.yPos, batch);
-            bulletArrayList.add(bullet);
-        }
         if (Gdx.input.isKeyPressed(Input.Keys.W) && canPlayerMove(3)) {
             yPos += 2;
             displaySprite = spriteUp;
+            direction = 3;
+            if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+                Bullet bullet = new Bullet(this.xPos, this.yPos, batch, direction);
+                bulletArrayList.add(bullet);
+            }
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.S) && canPlayerMove(4)) {
             yPos -= 2;
             displaySprite = sprite;
+            direction = 4;
+            if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+                Bullet bullet = new Bullet(this.xPos, this.yPos, batch, direction);
+                bulletArrayList.add(bullet);
+            }
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.A) && canPlayerMove(2)) {
             xPos -= 2;
             displaySprite = spriteLeft;
+            direction = 2;
+            if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+                Bullet bullet = new Bullet(this.xPos, this.yPos, batch, direction);
+                bulletArrayList.add(bullet);
+            }
             return;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D) && canPlayerMove(1)) {
             xPos += 2;
             displaySprite = spriteRight;
+            direction = 1;
+            if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+                Bullet bullet = new Bullet(this.xPos, this.yPos, batch, direction);
+                bulletArrayList.add(bullet);
+            }
             return;
         }
-        displaySprite = sprite;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            Bullet bullet = new Bullet(this.xPos, this.yPos, batch, direction);
+            bulletArrayList.add(bullet);
+        }
 
     }
 
     public boolean canPlayerMove(int direction) {
-        if (direction == 1 && xPos + 100 >= MainGameClass.WIDTH) return false;
-        if (direction == 2 && xPos <= 0) return false;
+        if (direction == 1 && xPos + 132 >= MainGameClass.WIDTH) return false;
+        if (direction == 2 && xPos <= 32) return false;
         if (direction == 3 && yPos + 100 >= MainGameClass.HEIGHT) return false;
-        if (direction == 4 && yPos <= 0) return false;
+        if (direction == 4 && yPos <= 25) return false;
         return true;
     }
+
+//    public void shoot(float direction) {
+//        Bullet bullet = new Bullet(this.xPos, this.yPos, batch);
+//        bulletArrayList.add(bullet);
+//    }
 
     public Sprite getSprite() {
         return displaySprite;
